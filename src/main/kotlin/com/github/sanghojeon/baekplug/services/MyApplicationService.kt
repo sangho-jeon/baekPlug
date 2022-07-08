9ca6023b2dfd363e5b7887e187b4220773188c1e
GitHub Action <action@github.com>
2022-07-08 06:02:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/sanghojeon/baekplug/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/sanghojeon/baekplug/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.sanghojeon.baekplug.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sanghojeon.baekplug.MyBundle
 
 class MyApplicationService {
 
